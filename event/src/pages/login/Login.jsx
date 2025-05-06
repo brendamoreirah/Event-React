3320093461729ca2ee8ab54c425e8ddc572270c1
Brenda Moreira <brenda.m.moreira6@aluno.senai.br>
2025-05-06 11:15:52 -0300

    feat: subindo a tela login

---
+++ event/src/pages/login/Login.jsx
@@ -0,0 +1,34 @@
+import Logo from "../../assets/img/logo1.svg"
+import "./Login.css"
+import Botao from "../../components/botao/Botao"
+
+
+const Login = () => {
+
+    return (
+        <main className="main_login">
+            <div className="banner"></div>
+            <section className="section_login">
+                <img src={Logo} alt="Logo do Event" />
+                <form action="" className="formulario_login">
+                
+                    <div className="campos_login">
+                        <div className="campo_input">
+                        <input type="username" name="nome"  placeholder="Digite seu nome"/>
+                        </div>
+                        <div className="campo_input">
+                    
+                        <input type="password"  name="Password" placeholder="Digite sua senha"/>
+                        </div>
+                    </div>
+                    <a href="">Esqueceu a senha?</a>
+                    <Botao nomeDoBotao="Entar" />
+      
+                </form>
+            </section>
+        </main>
+
+
+    )
+}
+export default Login;
